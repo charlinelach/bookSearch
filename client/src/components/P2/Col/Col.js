6c130c3a7ab7031997011b66dd06900c834bb39f
Charline <charlinerlach@gmail.com>
2020-11-30 20:02:53 -0600

    Updates to src components

--- client/src/components/P2/Col/Col.js
+++ client/src/components/P2/Col/Col.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Col(props) {
-  // const size = props.size.split(" ").map(size => "col-" + size).join(" ");
+  const size = props.size.split(" ").map(size => "col-" + size).join(" ");
 
   return <div className="col">{props.children}</div>;
 }
