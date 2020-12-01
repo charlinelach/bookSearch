d074c9456f713c2b756807908af6e21cdad8d1cb
Charline <charlinerlach@gmail.com>
2020-11-30 20:48:47 -0600

    Pages attempt

--- client/src/pages/Saved.js
+++ client/src/pages/Saved.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import API from "../utils/API";
-import Results from "../components/Results/Results";
+
+import Card from "../components/Card";
+import Col from "../components/Col";
+import Container from "../components/Container";
+import Row from "../components/Row";
+import Saved from "../components/Saved";
 
 class Saved extends Component {
     state = {
@@ -8,17 +13,53 @@ class Saved extends Component {
     }
 
     componentDidMount() {
-        API.savedBooks()
-            .then(savedBooks => this.setState({ savedBooks: savedBooks }))
+        API.getBooks()
+            .then(res => this.setState({ books: res.data }))
             .catch(err => console.error(err));
     }
 
+    loadBooks = () => {
+        API.getBooks()
+          .then(res =>
+            this.setState({ books: res.data })
+          )
+          .catch(err => console.log(err));
+      };
+    
+      handleDeleteBook = id => {
+        API.deleteBook(id)
+          .then(res => this.loadBooks())
+          .catch(err => console.log(err));
+      }
+
     render() {
         return (
-            <div className="container">
-                <h2>Saved books</h2>
-                <Results books={this.state.savedBooks} />
-            </div>
+<Container>
+        <Row>
+          <Col size="md-12">
+            {this.state.books.length ? (
+              <Card heading="Saved Books">
+                {this.state.books.map(book => (
+                  <Saved
+                    key={book._id}
+                    src={book 
+                      ? book.src 
+                      : "http://icons.iconarchive.com/icons/paomedia/small-n-flat/128/book-icon.png"}
+                    title={book.title}
+                    authors={book.authors.join(", ")}
+                    date={book.date}
+                    description={book.description}
+                    link={book.link}
+                    handleDeleteBook={() => this.handleDeleteBook(book._id)}
+                  />
+                ))}
+              </Card>
+            ) : (
+              <Card heading="Saved Books"></Card>
+            )}
+          </Col>
+        </Row>
+      </Container>
         )
     }
 }
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,7 +1,14 @@
 import React from "react";
-import Form from "../components/Form/Form";
-import Results from "../components/Results/Results";
 import API from "../utils/API";
+
+import Card from "../components/Card/Card";
+import Col from "../components/Col/Col";
+import Container from "../components/Container/Container";
+import Form from "../components/Form/Form";
+import Jumbo from "../components/Jumbo/Jumbo";
+import Results from "../components/Results/Results";
+import Row from "../components/Row/Row";
+
 
 class Search extends React.Component {
     state = {
@@ -9,25 +16,24 @@ class Search extends React.Component {
         books: []
     };
 
-    componentDidMount() {
-        this.searchBook();
-    }
+    searchBooks = query => {
+        API.searchBooks(query)
+            .then(res =>
+                this.setState(
+                    {
+                        books: res.data.items,
+                        search: ""
+                    },
+                    console.log(res.data.items)
+                )
+            )
+            .catch(err => console.log(err));
+    };
 
-    makeBook = bookData => {
-        return {
-            _id: bookData.id,
-            title: bookData.volumeInfo.title,
-            authors: bookData.volumeInfo.authors,
-            description: bookData.volumeInfo.description,
-            image: bookData.volumeInfo.imageLinks.thumbnail,
-            link: bookData.volumeInfo.previewLink
-        }
-    }
 
-    searchBook = query => {
-        API.getBook(query)
-            .then(res => this.setState({ books: res.data.items.map(bookData => this.makeBook(bookData)) }))
-            .catch(err => console.error(err));
+    handleFormSubmit = event => {
+        event.preventDefault();
+        this.searchBooks(this.state.search);
     };
 
     handleInputChange = event => {
@@ -38,25 +44,97 @@ class Search extends React.Component {
         });
     };
 
-    handleFormSubmit = event => {
-        event.preventDefault();
-        this.searchBook(this.state.search);
+    deleteBook = id => {
+        API.deleteBook(id)
+            .then(res => console.log(res.status))
+            .catch(err => console.log(err));
     };
+
+    // saves book to database
+    handleSaveBook = bookData => {
+        API.saveBook(bookData)
+            .then(res => alert("Book Saved!"))
+            .catch(err => console.log(err));
+    };
+
+    // componentDidMount() {
+    //     this.searchBook();
+    // }
+
+    // makeBook = bookData => {
+    //     return {
+    //         _id: bookData.id,
+    //         title: bookData.volumeInfo.title,
+    //         authors: bookData.volumeInfo.authors,
+    //         description: bookData.volumeInfo.description,
+    //         image: bookData.volumeInfo.imageLinks.thumbnail,
+    //         link: bookData.volumeInfo.previewLink
+    //     }
+    // }
+
+    // searchBook = query => {
+    //     API.getBook(query)
+    //         .then(res => this.setState({ books: res.data.items.map(bookData => this.makeBook(bookData)) }))
+    //         .catch(err => console.error(err));
+    // };
+
 
     render() {
         return (
-            <div>
-                <Form
-                    search={this.state.search}
-                    handleInputChange={this.handleInputChange}
-                    handleFormSubmit={this.handleFormSubmit}
-                />
-                <div className="container">
-                    <h2>Results</h2>
-                    <Results books={this.state.books} />
-                </div>
-            </div>
-        )
+            <Container>
+                <Row>
+                    <Col size="md-12">
+                        <Jumbo />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size="md-12">
+                        <Card heading="Google's Book Spy">
+                            <Form
+                                value={this.state.search}
+                                handleInputChange={this.handleInputChange}
+                                handleFormSubmit={this.handleFormSubmit}
+                            />
+                        </Card>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size="md-12">
+                        {this.state.books.length ? (
+                            <Card heading="Results">
+                                {this.state.books.map(book => (
+                                    <Results
+                                        key={book.id}
+                                        src={book.volumeInfo.imageLinks
+                                            ? book.volumeInfo.imageLinks.thumbnail
+                                            : "http://icons.iconarchive.com/icons/paomedia/small-n-flat/128/book-icon.png"}
+                                        title={book.volumeInfo.title}
+                                        authors={book.volumeInfo.authors
+                                            ? book.volumeInfo.authors.join(", ")
+                                            : "N/A"}
+                                        date={book.volumeInfo.publishedDate}
+                                        description={book.volumeInfo.description}
+                                        link={book.volumeInfo.infoLink}
+                                        handleSaveBook={() => this.handleSaveBook({
+                                            title: book.volumeInfo.title,
+                                            src: book.volumeInfo.imageLinks
+                                                ? book.volumeInfo.imageLinks.thumbnail
+                                                : "http://icons.iconarchive.com/icons/paomedia/small-n-flat/128/book-icon.png",
+                                            authors: book.volumeInfo.authors,
+                                            date: book.volumeInfo.publishedDate,
+                                            description: book.volumeInfo.description,
+                                            link: book.volumeInfo.infoLink
+                                        })}
+                                    />
+                                ))}
+                            </Card>
+                        ) : (
+                                <Card heading="Search Results"></Card>
+                            )}
+                    </Col>
+                </Row>
+            </Container>
+        );
     }
 }
 
